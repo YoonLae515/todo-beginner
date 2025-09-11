888a5d6a0b605296565c3a4ee2f1818ea04adc81
YoonLae515 <laewah1234567@gmail.com>
2025-09-11 14:35:39 +0700

    Add cross icon JS,CSS

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span"); //Add cross icon
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
     inputBox.value = "";
 }
